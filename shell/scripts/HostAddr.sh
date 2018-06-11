e22821be2a84323c7febd9008f2fb713b8a37c58
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-06-11 22:00:00 +0900

    Implement `HostAddr` script

---
+++ shell/scripts/HostAddr.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+HOST=${1:-`hostname`}
+HOST=`echo $HOST | sed -e 's/\..*//'`
+
+cat /etc/hosts |
+sed -e 's/#.*//' -e 's/	/ /g' -e 's/  */ /g' -e 's/ *$//g' -e 's/^ *//g' -e "s/ $HOST[. ].*/ $HOST/g" |
+sed -n "/ $HOST$/p" |
+sed -e 's/ .*//'
+
+exit 0
