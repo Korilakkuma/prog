2eafab7215034f5b40cf3b9c417ef15a2412f568
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-05 22:00:00 +0900

    `wait`

---
+++ c/system-call/process/wait.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+void getcmd(char *cmd);
+
+int main(void) {
+    char cmd[1024];
+    int status;
+    pid_t pid;
+
+    while (1) {
+        fputs("% ", stdout);
+        getcmd(cmd);
+
+        if ((pid = fork()) == -1) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            execlp(cmd, cmd, (char *)0);
+            perror(cmd);
+            exit(1);
+        } else {
+            if (wait(&status) == (pid_t)-1) {
+                perror("wait");
+                exit(1);
+            }
+        }
+    }
+
+    exit(0);
+}
+
+void getcmd(char *cmd) {
+    int len;
+    char *p;
+
+    fgets(cmd, sizeof(cmd), stdin);
+
+    len = strlen(cmd) + 1;
+
+    if ((p = strchr(cmd, '\n')) == NULL) {
+        cmd[len - 1] = '\0';
+    } else {
+        *p = '\0';
+    }
+}
