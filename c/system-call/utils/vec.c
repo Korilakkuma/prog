0f2cc4391cd58770b3d4daa22f374622de66b114
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-09 22:00:00 +0900

    vec

---
+++ c/system-call/utils/vec.c
@@ -0,0 +1,54 @@
+#include <ctype.h>
+#include "vec.h"
+
+int strtovec(char *s, char **v, int max) {
+    int i = 0;
+    int skip;
+
+    if ((max < 1) || (v == 0)) {
+        skip = 1;
+    } else {
+        skip = 0;
+    }
+
+    while (1) {
+        if (!skip && (i >= (max - 1))) {
+            v[i] = 0;
+            skip = 1;
+        }
+
+        while ((*s != '\0') && isspace(*s)) {
+            s++;
+        }
+
+        if (*s == '\0') {
+            break;
+        }
+
+        if (!skip) {
+            v[i] = s;
+        }
+
+        i++;
+
+        while ((*s != '\0') && !isspace(*s)) {
+            s++;
+        }
+
+        if (*s == '\0') {
+            break;
+        }
+
+        *s = '\0';
+
+        s++;
+    }
+
+    if (!skip) {
+        v[i] = 0;
+    }
+
+    i++;
+
+    return i;
+}
---
+++ c/system-call/utils/vec.h
@@ -0,0 +1,6 @@
+#ifndef _VEC_H_
+#define _VEC_H_
+
+extern int strtovec(char *s, char **v, int max);
+
+#endif
