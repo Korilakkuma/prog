889a54c3760cde5cab1e329d5db88cd2a81f1030
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-04-14 18:00:00 +0900

    onlcr by `termios`

---
+++ c/system-call/terminal/onlcr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+int main(void) {
+    int c;
+    struct termios new, old;
+
+    tcgetattr(STDIN_FILENO, &old);
+
+    new = old;
+
+    new.c_oflag &= ~ONLCR;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &new);
+
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &old);
+
+    return 0;
+}
