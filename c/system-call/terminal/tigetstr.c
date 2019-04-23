d445a2a89be6eb67689f0e53fa31ae100e329b77
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-04-23 22:00:00 +0900

    terminfo

---
+++ c/system-call/terminal/tigetstr.c
@@ -0,0 +1,15 @@
+#include <curses.h>
+#include <term.h>
+
+int main(void) {
+    char *p;
+
+    setupterm(NULL, 0, NULL);
+
+    p = tigetstr("cup");
+    p = tparm(p, 10, 20);
+
+    putp(p);
+
+    return 0;
+}
