7654179ceebf0a1462500c469c90ba00f0af81c3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-05-16 20:19:29 +0900

    fibonacci function

---
+++ c++/lesson/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n);
+
+int main() {
+    for (int n = 1; n <= 10; ++n) {
+        cout << "f(" << n << ") = " << fibonacci(n) << endl;
+    }
+}
+
+int fibonacci(int n) {
+    if (n < 3) {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
