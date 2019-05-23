15d6da8c1af8bb66d67c42f6dd7dcac1ffc13298
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-05-23 21:10:00 +0900

    `capacity`

---
+++ c++/lesson/vector_capacity.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v;
+
+    for (int i = 0; i < 100; i++) {
+        cout << v.capacity() << ", ";
+        v.push_back(i);
+    }
+
+    cout << endl;
+}
