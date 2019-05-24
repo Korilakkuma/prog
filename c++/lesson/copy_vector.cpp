e23b0eeff8c24f26c03ab1979c1d3fac795f02d6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-05-24 20:22:49 +0900

    copy vector

---
+++ c++/lesson/copy_vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v1{ 2, 3, 5, 7 };
+
+    auto v2(v1);  // or auto v2 = v1;
+
+    v1[0] = 100;
+
+    for (auto x : v2) {
+        cout << x << endl;
+    }
+}
