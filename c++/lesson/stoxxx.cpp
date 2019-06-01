787abe0728a3ab2efae23a54c330f3d1dada7956
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-06-01 17:30:00 +0900

    `stoxxx`

---
+++ c++/lesson/stoxxx.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string stri = "561";
+    int i = stoi(stri);
+
+    cout << (i + 1) << endl;
+
+    string strd = "0.31415";
+    double d = stod(strd);
+
+    cout << (d * 10) << endl;
+}
