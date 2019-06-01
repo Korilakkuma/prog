1b9ce2a41c139e3b36d4fc9029c782c6af70e675
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-06-01 22:56:00 +0900

    `sregex_iterator`

---
+++ c++/lesson/sregex_iterator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    string str ="My postal code is 194-0013, her postal code is 153-0042";
+
+    regex rx(R"(\d{3}-\d{4})");
+
+    sregex_iterator it(str.begin(), str.end(), rx);
+    sregex_iterator end;
+
+    while (it != end) {
+        cout << (it++)->str() << endl;
+    }
+}
