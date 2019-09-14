795659a9e7d8f94d5015462353e944a38b7434b5
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-09-14 17:00:02 +0900

    Oscillator (sine, sawtooth, triangle)

---
+++ c/dsp/oscillator/sawtooth.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../wave/wave.h"
+
+enum {
+  N = 15
+};
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fputs("Require amplitude and f0\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  MONO_PCM pcm;
+  char *endptr;
+
+  double a  = strtod(argv[1], &endptr);
+  double f0 = strtod(argv[2], &endptr);
+
+  pcm.fs = 8000;
+  pcm.bits = 16;
+  pcm.length = 8000;
+
+  if ((pcm.s = (double *)calloc(pcm.length, sizeof(double))) == NULL) {
+    perror("calloc");
+    exit(EXIT_FAILURE);
+  }
+
+  for (int n = 0; n < pcm.length; n++) {
+    for (int i = 1; i <= N; i++) {
+      pcm.s[n] += a * sin(2.0 * M_PI * (i * f0) * n / pcm.fs);
+    }
+  }
+
+  mono_wave_write(&pcm, "sawtooth.wav");
+
+  free(pcm.s);
+
+  return 0;
+}
---
+++ c/dsp/oscillator/sine.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../wave/wave.h"
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fputs("Require amplitude and f0\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  MONO_PCM pcm;
+  char *endptr;
+
+  double a  = strtod(argv[1], &endptr);
+  double f0 = strtod(argv[2], &endptr);
+
+  pcm.fs = 8000;
+  pcm.bits = 16;
+  pcm.length = 8000;
+
+  if ((pcm.s = (double *)calloc(pcm.length, sizeof(double))) == NULL) {
+    perror("calloc");
+    exit(EXIT_FAILURE);
+  }
+
+  for (int n = 0; n < pcm.length; n++) {
+    pcm.s[n] = a * sin(2.0 * M_PI * f0 * n / pcm.fs);
+  }
+
+  mono_wave_write(&pcm, "sine.wav");
+
+  free(pcm.s);
+
+  return 0;
+}
