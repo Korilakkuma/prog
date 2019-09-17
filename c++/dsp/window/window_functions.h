8432e06427b55ce83269b3f4c25622796b77d3fe
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-09-17 20:00:00 +0900

    Hanning window

---
+++ c++/dsp/window/window_functions.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include <cmath>
+
+std::vector<double> Hanning_window(int N) {
+  std::vector<double> w(N);
+
+  if ((N % 2) == 0) {
+    for (int n = 0; n < N; n++) {
+      w[n] = 0.5 - (0.5 * std::cos((2.0 * M_PI * n) / N));
+    }
+  } else {
+    for (int n = 0; n < N; n++) {
+      w[n] = 0.5 - (0.5 * std::cos((2.0 * M_PI * (n - 0.5)) / N));
+    }
+  }
+
+  return w;
+}
