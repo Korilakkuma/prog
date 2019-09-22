6b04b208e4648da4ed0ce364bb1b1b4adcaceec0
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-09-22 22:56:00 +0900

    Fix hanning window

--- c++/dsp/window/window_functions.h
+++ c++/dsp/window/window_functions.h
@@ -1,9 +1,7 @@
 #include <vector>
 #include <cmath>
 
-std::vector<double> hanning_window(int N) {
-  std::vector<double> w(N);
-
+void hanning_window(std::vector<double> &w, int N) {
   if ((N % 2) == 0) {
     for (int n = 0; n < N; n++) {
       w[n] = 0.5 - (0.5 * std::cos((2.0 * M_PI * n) / N));
@@ -13,6 +11,4 @@ std::vector<double> hanning_window(int N) {
       w[n] = 0.5 - (0.5 * std::cos((2.0 * M_PI * (n - 0.5)) / N));
     }
   }
-
-  return w;
 }
