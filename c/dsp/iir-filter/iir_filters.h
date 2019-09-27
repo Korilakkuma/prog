174690d5266f5e47750055e3d227e0dc4ebb45a8
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-09-27 18:00:00 +0900

    Add low-shelving, high-shelving, peaking

--- c/dsp/iir-filter/iir_filters.h
+++ c/dsp/iir-filter/iir_filters.h
@@ -7,7 +7,7 @@ void IIR_LPF(double fc, double Q, double *a, double *b) {
 
   a[0] = 1.0;
   a[1] = ((8.0 * pow(M_PI, 2) * pow(fc, 2)) - 2.0) / numerator;
-  a[2] = (1.0 - (2.0 * M_PI * fc) / Q + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
   b[0] = (4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
   b[1] = (8.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
   b[2] = (4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
@@ -20,7 +20,7 @@ void IIR_HPF(double fc, double Q, double *a, double *b) {
 
   a[0] = 1.0;
   a[1] = ((8.0 * pow(M_PI, 2) * pow(fc, 2)) - 2.0) / numerator;
-  a[2] = (1.0 - (2.0 * M_PI * fc) / Q + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
   b[0] = 1 / numerator;
   b[1] = -2 / numerator;
   b[2] = 1 / numerator;
@@ -33,7 +33,7 @@ void IIR_BPF(double fc, double Q, double *a, double *b) {
 
   a[0] = 1.0;
   a[1] = ((8.0 * pow(M_PI, 2) * pow(fc, 2)) - 2.0) / numerator;
-  a[2] = (1.0 - (2.0 * M_PI * fc) / Q + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
   b[0] = (2 * M_PI * fc / Q) / numerator;
   b[1] = 0;
   b[2] = -(2 * M_PI * fc / Q) / numerator;
@@ -46,8 +46,47 @@ void IIR_BEF(double fc, double Q, double *a, double *b) {
 
   a[0] = 1.0;
   a[1] = ((8.0 * pow(M_PI, 2) * pow(fc, 2)) - 2.0) / numerator;
-  a[2] = (1.0 - (2.0 * M_PI * fc) / Q + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
   b[0] = (4 * pow(M_PI, 2) * pow(fc, 2) + 1) / numerator;
   b[1] = (8 * pow(M_PI, 2) * pow(fc, 2) - 2) / numerator;
   b[2] = (4 * pow(M_PI, 2) * pow(fc, 2) + 1) / numerator;
+}
+
+void IIR_LOW_SHELVING(double fc, double Q, double g, double *a, double *b) {
+  fc = tan(M_PI * fc) / (2.0 * M_PI);
+
+  double numerator = 1.0 + ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2));
+
+  a[0] = 1.0;
+  a[1] = ((8.0 * pow(M_PI, 2) * pow(fc, 2)) - 2.0) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
+  b[0] = (1 + sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
+  b[1] = (8.0 * pow(M_PI, 2) * pow(fc, 2) * (1 + g) - 2) / numerator;
+  b[2] = (1 - sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
+}
+
+void IIR_HIGH_SHELVING(double fc, double Q, double g, double *a, double *b) {
+  fc = tan(M_PI * fc) / (2.0 * M_PI);
+
+  double numerator = 1.0 + ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2));
+
+  a[0] = 1.0;
+  a[1] = ((8.0 * pow(M_PI, 2) * pow(fc, 2)) - 2.0) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
+  b[0] = ((1 + g) + sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
+  b[1] = (8 * pow(M_PI, 2) * pow(fc, 2) - 2 * (1 + g)) / numerator;
+  b[2] = ((1 + g) - sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
+}
+
+void IIR_PEAKING(double fc, double Q, double g, double *a, double *b) {
+  fc = tan(M_PI * fc) / (2.0 * M_PI);
+
+  double numerator = 1.0 + ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2));
+
+  a[0] = 1.0;
+  a[1] = ((8.0 * pow(M_PI, 2) * pow(fc, 2)) - 2.0) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * pow(M_PI, 2) * pow(fc, 2))) / numerator;
+  b[0] = (1 + ((2 * M_PI * fc) / Q) * (1 + g) + 4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
+  b[1] = (8.0 * pow(M_PI, 2) * pow(fc, 2) - 2) / numerator;
+  b[2] = (1 - ((2 * M_PI * fc) / Q) * (1 + g) + 4.0 * pow(M_PI, 2) * pow(fc, 2)) / numerator;
 }
