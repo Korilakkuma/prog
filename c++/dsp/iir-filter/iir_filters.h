e28bc6822cb467245f4e66056124aba71c3b0939
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-09-27 20:00:00 +0900

    Add low-shelving, high-shelving, peaking

--- c++/dsp/iir-filter/iir_filters.h
+++ c++/dsp/iir-filter/iir_filters.h
@@ -52,3 +52,42 @@ void IIR_BEF(double fc, double Q, std::vector<double> &a, std::vector<double> &b
   b[1] = (8 * std::pow(M_PI, 2) * std::pow(fc, 2) - 2) / numerator;
   b[2] = (4 * std::pow(M_PI, 2) * std::pow(fc, 2) + 1) / numerator;
 }
+
+void IIR_LOW_SHELVING(double fc, double Q, double g, std::vector<double> &a, std::vector<double> &b) {
+  fc = std::tan(M_PI * fc) / (2.0 * M_PI);
+
+  double numerator = 1.0 + ((2.0 * M_PI * fc) / Q) + (4.0 * std::pow(M_PI, 2) * std::pow(fc, 2));
+
+  a[0] = 1.0;
+  a[1] = ((8.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) - 2.0) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * std::pow(M_PI, 2) * std::pow(fc, 2))) / numerator;
+  b[0] = (1 + std::sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / numerator;
+  b[1] = (8.0 * std::pow(M_PI, 2) * std::pow(fc, 2) * (1 + g) - 2) / numerator;
+  b[2] = (1 - std::sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / numerator;
+}
+
+void IIR_HIGH_SHELVING(double fc, double Q, double g, std::vector<double> &a, std::vector<double> &b) {
+  fc = std::tan(M_PI * fc) / (2.0 * M_PI);
+
+  double numerator = 1.0 + ((2.0 * M_PI * fc) / Q) + (4.0 * std::pow(M_PI, 2) * std::pow(fc, 2));
+
+  a[0] = 1.0;
+  a[1] = ((8.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) - 2.0) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * std::pow(M_PI, 2) * std::pow(fc, 2))) / numerator;
+  b[0] = ((1 + g) + std::sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / numerator;
+  b[1] = (8 * std::pow(M_PI, 2) * std::pow(fc, 2) - 2 * (1 + g)) / numerator;
+  b[2] = ((1 + g) - std::sqrt(1 + g) * ((2 * M_PI * fc) / Q) + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / numerator;
+}
+
+void IIR_PEAKING(double fc, double Q, double g, std::vector<double> &a, std::vector<double> &b) {
+  fc = std::tan(M_PI * fc) / (2.0 * M_PI);
+
+  double numerator = 1.0 + ((2.0 * M_PI * fc) / Q) + (4.0 * std::pow(M_PI, 2) * std::pow(fc, 2));
+
+  a[0] = 1.0;
+  a[1] = ((8.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) - 2.0) / numerator;
+  a[2] = (1.0 - ((2.0 * M_PI * fc) / Q) + (4.0 * std::pow(M_PI, 2) * std::pow(fc, 2))) / numerator;
+  b[0] = (1 + ((2 * M_PI * fc) / Q) * (1 + g) + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / numerator;
+  b[1] = (8.0 * std::pow(M_PI, 2) * std::pow(fc, 2) - 2) / numerator;
+  b[2] = (1 - ((2 * M_PI * fc) / Q) * (1 + g) + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / numerator;
+}
