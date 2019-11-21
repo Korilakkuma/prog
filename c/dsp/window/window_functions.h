a9e3cc73362cca811e8dd730644951551d476736
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-11-21 20:52:00 +0900

    Add vorbis window

--- c/dsp/window/window_functions.h
+++ c/dsp/window/window_functions.h
@@ -17,3 +17,9 @@ void sine_window(double *w, int N) {
     w[n] = sin(M_PI * ((double)n / N));
   }
 }
+
+void vorbis_window(double *w, int N) {
+  for (int n = 0; n < N; n++) {
+    w[n] = sin((M_PI / 2.0) * pow(sin(M_PI * ((double)n / N)), 2.0));
+  }
+}
