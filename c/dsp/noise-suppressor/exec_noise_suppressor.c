144400d96613e23a62ee620abe6728fa449627af
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-01-16 22:47:36 +0900

    Noise Suppressor

---
+++ c/dsp/noise-suppressor/exec_noise_suppressor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../wave/wave.h"
+#include "noise_suppressor.h"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("Require threshold\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  STEREO_PCM pcm0, pcm1;
+
+  double threshold = strtod(argv[1], NULL);
+
+  stereo_wave_read(&pcm0, "stereo.wav");
+
+  pcm1.fs     = pcm0.fs;
+  pcm1.bits   = pcm0.bits;
+  pcm1.length = pcm0.length;
+  pcm1.sL     = (double *)calloc(pcm1.length, sizeof(double));
+  pcm1.sR     = (double *)calloc(pcm1.length, sizeof(double));
+
+  NoiseSupressor(threshold, pcm0.sL, pcm1.sL, pcm1.fs, pcm1.length);
+  NoiseSupressor(threshold, pcm0.sR, pcm1.sR, pcm1.fs, pcm1.length);
+
+  stereo_wave_write(&pcm1, "noise-suppressor.wav");
+
+  free(pcm0.sL);
+  free(pcm1.sL);
+  free(pcm0.sR);
+  free(pcm1.sR);
+
+  return 0;
+}
---
+++ c/dsp/noise-suppressor/noise_suppressor.h
@@ -0,0 +1,67 @@
+#include <math.h>
+#include "../window/window_functions.h"
+#include "../fft/fft.h"
+
+enum {
+  N = 1024
+};
+
+void NoiseSupressor(double threshold, double *in, double *out, int fs, int length) {
+  int number_of_frame = (length - (N / 2)) / (N / 2);
+
+  double *w      = (double *)calloc(N, sizeof(double));
+  double *x_real = (double *)calloc(N, sizeof(double));
+  double *x_imag = (double *)calloc(N, sizeof(double));
+  double *A      = (double *)calloc(N, sizeof(double));
+  double *T      = (double *)calloc(N, sizeof(double));
+  double *y_real = (double *)calloc(N, sizeof(double));
+  double *y_imag = (double *)calloc(N, sizeof(double));
+
+  hanning_window(w, N);
+
+  for (int frame = 0; frame < number_of_frame; frame++) {
+    int offset = (N / 2) * frame;
+
+    for (int n = 0; n < N; n++) {
+      x_real[n] = in[offset + n] * w[n];
+      x_imag[n] = 0.0;
+    }
+
+    FFT(x_real, x_imag, N);
+
+    for (int k = 0; k < N; k++) {
+      A[k] = sqrt((x_real[k] * x_real[k]) + (x_imag[k] * x_imag[k]));
+
+      if ((x_imag[k] != 0.0) && (x_real[k] != 0.0)) {
+        T[k] = atan2(x_imag[k], x_real[k]);
+      }
+    }
+
+    for (int k = 0; k < N; k++) {
+      A[k] -= threshold;
+
+      if (A[k] < 0.0) {
+        A[k] = 0.0;
+      }
+    }
+
+    for (int k = 0; k < N; k++) {
+      y_real[k] = A[k] * cos(T[k]);
+      y_imag[k] = A[k] * sin(T[k]);
+    }
+
+    IFFT(y_real, y_imag, N);
+
+    for (int n = 0; n < N; n++) {
+      out[offset + n] += y_real[n];
+    }
+  }
+
+  free(w);
+  free(x_real);
+  free(x_imag);
+  free(A);
+  free(T);
+  free(y_real);
+  free(y_imag);
+}
