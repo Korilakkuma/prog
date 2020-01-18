cd8feeefc0dc59d2bc95fcc33fa66961c4ce2b5b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-01-18 17:25:00 +0900

    Time Stretch (fast and slow)

--- c/dsp/time-stretch/exec_time_stretch.c
+++ c/dsp/time-stretch/exec_time_stretch.c
@@ -18,12 +18,12 @@ int main(int argc, char **argv) {
 
   pcm1.fs     = pcm0.fs;
   pcm1.bits   = pcm0.bits;
-  pcm1.length = pcm0.length;
+  pcm1.length = (int)(pcm0.length / rate) + 1;
   pcm1.sL     = (double *)calloc(pcm1.length, sizeof(double));
   pcm1.sR     = (double *)calloc(pcm1.length, sizeof(double));
 
-  TimeStretch(rate, pcm0.sL, pcm1.sL, pcm1.fs, pcm1.length);
-  TimeStretch(rate, pcm0.sR, pcm1.sR, pcm1.fs, pcm1.length);
+  TimeStretch(rate, pcm0.sL, pcm1.sL, pcm1.fs, pcm0.length);
+  TimeStretch(rate, pcm0.sR, pcm1.sR, pcm1.fs, pcm0.length);
 
   stereo_wave_write(&pcm1, "time-stretch.wav");
 
